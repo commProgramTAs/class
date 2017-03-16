3b29b599725545a176aca91ca854d743ab54702b
CharlieChen <new791104@gmail.com>
2017-03-16 14:36:41 +0800

    update

--- lab3/projects/lab3-CodeBlocks/example/ex1.c
+++ lab3/projects/lab3-CodeBlocks/example/ex1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int ex1(){
-    int n, i;
-
-    printf("Enter n(0 < n < 10,000): ");
-    scanf("%d", &n);
-    for (i = 1; i <= 10; i++) {
-        printf("%d\n", n * i);
-    }
+int ex1()
+{
+srand(time(NULL));
+int i=10;
+    while (i --> 0)
+        printf("%d\n",rand());
 
     return 0;
 }
+
