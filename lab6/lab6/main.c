f0a89d0c5463446a8d5320cc515a825809fd1379
CharlieChen <new791104@gmail.com>
2017-04-29 21:47:43 +0800

    no message

--- lab6/lab6/main.c
+++ lab6/lab6/main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void example(int);
+int ex;
+
 int main(){
     char input[20] = "";
-
+    ex = 3;
     do {
         printf("lab6 example: ");
         scanf("%s", &input);
@@ -20,9 +23,17 @@ int main(){
         else if (!strcmp(input, "min")) {
             min_f();
         }
+        else if (!strcmp(input, "example")) {
+            example(1);
+        }
     } while (strcmp(input, "exit"));
 
     system("pause");
 
     return 0;
+}
+
+void example(int a) {
+    printf("%d\n", a);
+    printf("ex: %d\n", ex);
 }
